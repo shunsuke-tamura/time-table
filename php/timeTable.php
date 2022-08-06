6397d9c82bb8a4b5b03a94b946ce78eb257a4f03
shunsuke-tamura <wwwxx4869@gmail.com>
2022-08-06 19:54:33 +0900

    fix: dbのPATHを修正

--- php/timeTable.php
+++ php/timeTable.php
@@ -1,6 +1,6 @@
 <?php 
 $json = file_get_contents("php://input");
 $contents = json_decode($json, true);
-$bytes = file_put_contents("./src/db/timeTable.json", $json); 
+$bytes = file_put_contents("../db/timeTable.json", $json); 
 echo "The number of bytes written are $bytes from post data.";
 ?>
